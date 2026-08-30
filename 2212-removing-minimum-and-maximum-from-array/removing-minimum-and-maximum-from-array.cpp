9c969f5482c03a35a1bf7fa2054407986f56f2bb
Ayush Kumar Singh <153955393+AyushDev007@users.noreply.github.com>
2026-08-30 23:44:59 +0530

    Time: 0 ms (100.00%) | Memory: 92.2 MB (4.89%) - LeetSync

---
+++ 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minimumDeletions(vector<int>& nums) {
+        int n = nums.size();
+        int mn = 0, mx = 0;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] < nums[mn]) mn = i;
+            if (nums[i] > nums[mx]) mx = i;
+        }
+        int a = min(mn, mx), b = max(mn, mx);
+        return min({b + 1, n - a, (a + 1) + (n - b)});
+    }
+};
